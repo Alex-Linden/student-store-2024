9e80b71e7afbfbebdc413b97fb504fdeb23912a3
Alex Linden <alextl2004@gmail.com>
2024-05-16 17:35:13 -0700

    start psql

---
+++ student_store.sql
@@ -0,0 +1,18 @@
+\echo 'Delete and recreate student_store db?'
+\prompt 'Return for yes or control-C to cancel > ' foo
+
+DROP DATABASE student_store;
+CREATE DATABASE student_store;
+\connect student_store
+
+\i student_store-schema.sql
+\i student_store-seed.sql
+
+\echo 'Delete and recreate student_store_test db?'
+\prompt 'Return for yes or control-C to cancel > ' foo
+
+DROP DATABASE student_store_test;
+CREATE DATABASE student_store_test;
+\connect student_store_test
+
+\i student_store-schema.sql
